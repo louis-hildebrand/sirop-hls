9a6507164cff2baa11897a52bc64844290006467
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-08-29 12:06:16 -0400

    Add pretty-printer to help with debugging

---
+++ src/main/scala/PrettyPrinter.scala
@@ -0,0 +1,86 @@
+import scala.jdk.CollectionConverters._
+
+object PrettyPrinter {
+  def show(
+      e: Expr
+  )(implicit numByParam: Map[Param, Int] = Map()): String = {
+    e match {
+      case True           => "True"
+      case False          => "False"
+      case IntCst(n)      => n.toString
+      case Add(x, y)      => s"(${show(x)}) + (${show(y)})"
+      case Sub(x, y)      => s"(${show(x)}) - (${show(y)})"
+      case Mul(x, y)      => s"(${show(x)}) * (${show(y)})"
+      case Div(x, y)      => s"(${show(x)}) / (${show(y)})"
+      case Mod(x, y)      => s"(${show(x)}) % (${show(y)})"
+      case Equal(x, y)    => s"(${show(x)}) === (${show(y)})"
+      case NotEqual(x, y) => s"(${show(x)}) !== (${show(y)})"
+      case LessThan(x, y) => s"(${show(x)}) < (${show(y)})"
+      case And(x, y)      => s"(${show(x)}) && (${show(y)})"
+      case IfThenElse(c, t, f) =>
+        s"""if (${show(c)}) then {
+           |${indent(show(t))}
+           |} else {
+           |${indent(show(f))}
+           |}
+           |""".stripMargin.stripTrailing
+      case p: Param =>
+        numByParam.get(p) match {
+          case Some(n) => s"p${n}"
+          case None    => p.toString
+        }
+      case Function(p, b) =>
+        val paramNum =
+          if numByParam.isEmpty then 0 else numByParam.values.max + 1
+        val pStr = show(p)(numByParam + (p -> paramNum))
+        val bStr = show(b)(numByParam + (p -> paramNum))
+        if isMultiline(bStr) then {
+          s"""(${pStr} /* ${p.toString} */) =>
+             |${indent(bStr)}
+             |""".stripMargin.stripTrailing
+        } else {
+          s"(${pStr} /* ${p.toString} */) => ${bStr}"
+        }
+      case FunCall(f, a) => s"(${show(f)})(${show(a)})"
+      case Tuple(elems: _*) =>
+        "(" + elems.map(e => show(e)).mkString(", ") + ")"
+      case TupleAccess(t, i) => s"${show(t)}.__${show(i)}"
+      case StmBuild(n, z, f) =>
+        val nStr = show(n)
+        val zStr = show(z)
+        val fStr = show(f)
+        if isMultiline(nStr) || isMultiline(zStr) || isMultiline(fStr) then {
+          s"""StmBuild(
+             |${indent(nStr)},
+             |${indent(zStr)},
+             |${indent(fStr)}
+             |)
+             |""".stripMargin.stripTrailing
+        } else {
+          s"StmBuild(${nStr}, ${zStr}, ${fStr})"
+        }
+      case StmNext(s)   => s"StmNext(${show(s)})"
+      case StmLength(s) => s"len(${show(s)})"
+      case VecBuild(n, f) =>
+        val nStr = show(n)
+        val fStr = show(f)
+        if isMultiline(nStr) || isMultiline(fStr) then {
+          s"""VecBuild(
+             |${indent(nStr)},
+             |${indent(fStr)}
+             |)
+             |""".stripMargin
+        } else {
+          s"VecBuild(${nStr}, ${fStr})"
+        }
+      case VecAccess(v, i) => s"${show(v)}[${show(i)}]"
+      case VecLength(v)    => s"len(${show(v)})"
+    }
+  }
+
+  private def indent(s: String): String = {
+    s.lines.map(l => s"    ${l}").iterator().asScala.mkString("\n")
+  }
+
+  private def isMultiline(s: String): Boolean = s.linesIterator.length > 1
+}
