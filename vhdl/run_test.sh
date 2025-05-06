3399651644da5a16ce8ac5f7d3379a1b24e061e6
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-05-06 16:59:24 -0400

    Make run_test.sh more user-friendly

--- vhdl/run_test.sh
+++ vhdl/run_test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# $1: Path to VHDL project directory
-# Additionally, pass -v or --verbose to see output from the synthesizer and simulator.
-
 set -u
 
 TIMEOUT='10s'
@@ -14,38 +11,83 @@ TESTBENCH_COMPILE_FAILED=5
 SIMULATION_TIMEOUT=6
 SIMULATION_FAILED=7
 
+function print_basic_usage {
+    echo "Usage: run_test.sh PROJ_DIR [-h|--help] [-v|--verbose] [-i|--interactive]"
+}
+
+function print_usage {
+    print_basic_usage
+    echo
+    echo "Arguments:"
+    echo "  PROJ_DIR           Path to the VHDL project directory"
+    echo "  -h, --help         Print the help message and exit"
+    echo "  -v, --verbose      Show the output from the VHDL compiler and simulator"
+    echo "  -i, --interactive  Open the simulator GUI"
+    echo
+    echo "Status codes:"
+    echo "  $BAD_ARGS  Bad args"
+    echo "  $MISSING_PROJ  Project could not be found"
+    echo "  $DESIGN_COMPILE_FAILED  The design could not be compiled"
+    echo "  $TESTBENCH_COMPILE_FAILED  The testbench could not be compiled"
+    echo "  $SIMULATION_TIMEOUT  The simulation timed out"
+    echo "  $SIMULATION_FAILED  The simulation failed (probably due to incorrect output from the design)"
+}
+
 function echoerr {
     >&2 echo "$1"
 }
 
-VERBOSE='false'
-for arg in "$@"; do
-    if [[ "$arg" = "--verbose" ]] || [[ "$arg" = "-v" ]]; then
-        VERBOSE='true'
-        break
-    fi
-done
-if [[ "$VERBOSE" = 'true' ]]; then
-    echo "Running in verbose mode."
-fi
-
-if [[ "$#" -lt 1 ]]; then
-    echoerr "Missing required argument PROJ_DIR."
+function usage_error {
+    >&2 print_basic_usage
+    echoerr ""
+    echoerr "$1"
     exit "$BAD_ARGS"
-fi
-PROJ_DIR="$1"
+}
+
+function parse_args {
+    for arg in "$@"; do
+        if [[ "$arg" = "--help" ]] || [[ "$arg" = "-h" ]]; then
+            print_usage
+            exit 0
+        fi
+    done
+
+    if [[ "$#" -lt 1 ]]; then
+        usage_error "Missing required argument PROJ_DIR."
+    fi
+    proj_dir="$1"
+    if [[ "$proj_dir" == -* ]]; then
+        usage_error "Flags must come after PROJ_DIR."
+    fi
+
+    verbose_mode='false'
+    interactive_mode='false'
+    for arg in "${@:2}"; do
+        if [[ "$arg" = "--verbose" ]] || [[ "$arg" = "-v" ]]; then
+            verbose_mode='true'
+        elif [[ "$arg" = "--interactive" ]] || [[ "$arg" = "-i" ]]; then
+            interactive_mode='true'
+        else
+            usage_error "Unrecognized argument: $arg"
+        fi
+    done
+}
 
 function compile {
   vcom -2008 "$1"
 }
 
 function run_simulation {
-    timeout "$TIMEOUT" vsim -c -do "run -all; quit -code [coverage attribute -name TESTSTATUS -concise]" -t 1ps -L altera -L lpm -L sgate -L altera_mf -L altera_lnsim -L cyclonev -L cyclonev_hssi -L work -voptargs="+acc" testbench
+    if [[ "$interactive_mode" == 'true' ]]; then
+        vsim -i -do "add wave sim:/testbench/DUT/*; run -all" -t 1ps -L altera -L lpm -L sgate -L altera_mf -L altera_lnsim -L cyclonev -L cyclonev_hssi -L work -voptargs="+acc" testbench
+    else
+        timeout "$TIMEOUT" vsim -c -do "run -all; quit -code [coverage attribute -name TESTSTATUS -concise]" -t 1ps -L altera -L lpm -L sgate -L altera_mf -L altera_lnsim -L cyclonev -L cyclonev_hssi -L work -voptargs="+acc" testbench
+    fi
 }
 
 function main {
-    cd "$PROJ_DIR" || {
-        echoerr "Failed to move to directory $PROJ_DIR. Does it exist?"
+    cd "$proj_dir" || {
+        echoerr "Failed to move to directory $proj_dir. Does it exist?"
         exit "$MISSING_PROJ"
     }
 
@@ -82,8 +124,9 @@ function main {
     }
 }
 
-if [[ "$VERBOSE" = "true" ]]; then
+parse_args "$@"
+if [[ "$verbose_mode" = "true" ]]; then
     main && echo "All good :D"
 else
-    main 2>&1 > /dev/null
+    main > /dev/null 2>&1
 fi
