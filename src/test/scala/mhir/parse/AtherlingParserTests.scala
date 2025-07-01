e970ed0b87884b35f4e42384a0e3246014680518
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-07-01 14:42:27 -0400

    Test parsing all Aetherling map benchmarks

--- src/test/scala/mhir/parse/AtherlingParserTests.scala
+++ src/test/scala/mhir/parse/AtherlingParserTests.scala
@@ -4,8 +4,37 @@ import mhir.ir._
 import mhir.ir.typecheck.TypeCheck
 import mhir.sugar.{Fifo, StmMap, VecMap}
 import org.scalatest.funsuite.AnyFunSuite
+import os.Path
 
 class AtherlingParserTests extends AnyFunSuite {
+
+  /** The directory containing benchmarks written in Aetherling's space-time
+    * language.
+    */
+  private val OriginalCodeDir: Path =
+    os.pwd / "src" / "test" / "resources" / "aetherling_benchmarks" / "original"
+
+  /** The directory containing the Aetherling benchmarks after parsing to an
+    * [[mhir.ir.Expr]].
+    */
+  private val ParsedCodeDir: Path =
+    os.pwd / "src" / "test" / "resources" / "aetherling_benchmarks" / "parsed"
+
+  /** Whether to save the parsed programs to [[ParsedCodeDir]].
+    *
+    * If false, then the programs in [[ParsedCodeDir]] will be used as the
+    * expected outputs and nothing will be written.
+    *
+    * If true, then the parsed programs will be saved to [[ParsedCodeDir]]. This
+    * is useful in case you want to change the expected outputs, just don't
+    * forget to review the outputs and change this field back to `false`.
+    */
+  private val SaveOutput: Boolean = false
+
+  test("SaveOutput") {
+    assert(!SaveOutput)
+  }
+
   test("Map:10/1") {
     val src =
       s"""module0 (I :: UInt8T) =
@@ -65,7 +94,24 @@ class AtherlingParserTests extends AnyFunSuite {
   }
 
   test("map") {
-    pending
+    val files = os
+      .list(OriginalCodeDir)
+      .filter(os.isFile)
+      .filter(p => p.baseName.startsWith("map"))
+    assert(files.nonEmpty)
+    for (f <- files) {
+      val code = os.read(f)
+      val parsed = AetherlingParser.parse(code)
+      val actual = mhir.debug.PrettyPrinter.show(parsed)
+      val expectedPath = ParsedCodeDir / f.baseName
+      if (SaveOutput) {
+        os.write(expectedPath, actual)
+      } else {
+        val expected = os.read(expectedPath)
+        assert(actual == expected)
+      }
+    }
+    assume(!SaveOutput)
   }
 
   test("conv2d") {
