0766db78411cd77e0bc111b6997f650fbdcfde13
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-07-15 12:16:28 -0400

    Test correctness of sum benchmarks

--- src/test/scala/mhir/main/aetherling/CompilerTests.scala
+++ src/test/scala/mhir/main/aetherling/CompilerTests.scala
@@ -13,129 +13,147 @@ class CompilerTests extends AnyFunSuite {
     os.pwd / "src" / "test" / "resources" / "aetherling_benchmarks" / "original"
   private val VhdlDir = os.pwd / "src" / "test" / "vhdl"
 
-  test("map:1") {
-    val inFile = BenchmarksDir / "map_1.txt"
-    val outDir = VhdlDir / "aetherling_map_1_test"
+  private def runTest(
+      benchmark: String,
+      simplify: Boolean,
+      input: StmLiteral,
+      expectedOutput: StmLiteral
+  ): Unit = {
+    val inFile = BenchmarksDir / s"$benchmark.txt"
+    val outDir = VhdlDir / s"aetherling_${benchmark}_test"
     if (os.exists(outDir)) os.remove.all(outDir)
-    val args = Args(inFile = inFile, outDir = outDir)
+    val args = Args(inFile = inFile, outDir = outDir, simplify = simplify)
     val f = Compiler.compile(args)
-    val input = TestInput(
-      (0 until 200).flatMap(t => Seq(None, Some(C((t * t) % 250)(U8))))
-    )
-    val expectedOutput = StmLiteral(
-      (0 until 200).map(t => C(5 + (t * t) % 250)(U8)): _*
-    )()
+    val testInput = TestInput(input.elems.map(e => Some(e)))
     TestbenchGenerator.makeTestbench(
-      inputsByVar = Map(f.asInstanceOf[Function].param -> input),
+      inputsByVar = Map(f.asInstanceOf[Function].param -> testInput),
       out = expectedOutput,
       dir = outDir
     )
     assert(TestRunner.testExistingProject(outDir) == TestPassed)
+  }
+
+  test("map:1") {
+    runTest(
+      "map_1",
+      simplify = true,
+      input = StmLiteral((0 until 200).map(t => C((t * t) % 250)(U8)): _*)(),
+      expectedOutput =
+        StmLiteral((0 until 200).map(t => C(5 + (t * t) % 250)(U8)): _*)()
+    )
   }
 
   test("map:1:no-simplify") {
-    val inFile = BenchmarksDir / "map_1.txt"
-    val outDir = VhdlDir / "aetherling_map_1_test_no_simplify"
-    if (os.exists(outDir)) os.remove.all(outDir)
-    val args = Args(inFile = inFile, outDir = outDir, simplify = false)
-    val f = Compiler.compile(args)
-    val input = TestInput(
-      (0 until 200).flatMap(t => Seq(None, Some(C((t * t) % 250)(U8))))
+    runTest(
+      "map_1",
+      simplify = false,
+      input = StmLiteral((0 until 200).map(t => C((t * t) % 250)(U8)): _*)(),
+      expectedOutput =
+        StmLiteral((0 until 200).map(t => C(5 + (t * t) % 250)(U8)): _*)()
     )
-    val expectedOutput = StmLiteral(
-      (0 until 200).map(t => C(5 + (t * t) % 250)(U8)): _*
-    )()
-    TestbenchGenerator.makeTestbench(
-      inputsByVar = Map(f.asInstanceOf[Function].param -> input),
-      out = expectedOutput,
-      dir = outDir
-    )
-    assert(TestRunner.testExistingProject(outDir) == TestPassed)
   }
 
   test("map:20") {
-    val inFile = BenchmarksDir / "map_20.txt"
-    val outDir = VhdlDir / "aetherling_map_20_test"
-    if (os.exists(outDir)) os.remove.all(outDir)
-    val args = Args(inFile = inFile, outDir = outDir)
-    val f = Compiler.compile(args)
-    val input = TestInput(
-      (0 until 10).flatMap(t =>
-        Seq(None, Some(VecLiteral((0 until 20).map(i => C(i + t)(U8)): _*)()))
-      )
+    runTest(
+      "map_20",
+      simplify = true,
+      input = StmLiteral(
+        (0 until 10).map(t =>
+          VecLiteral((0 until 20).map(i => C(i + t)(U8)): _*)()
+        ): _*
+      )(),
+      expectedOutput = StmLiteral(
+        (0 until 10).map(t =>
+          VecLiteral((0 until 20).map(i => C(i + t + 5)(U8)): _*)()
+        ): _*
+      )()
     )
-    val expectedOutput = StmLiteral(
-      (0 until 10).map(t =>
-        VecLiteral((0 until 20).map(i => C(i + t + 5)(U8)): _*)()
-      ): _*
-    )()
-    TestbenchGenerator.makeTestbench(
-      inputsByVar = Map(f.asInstanceOf[Function].param -> input),
-      out = expectedOutput,
-      dir = outDir
-    )
-    assert(TestRunner.testExistingProject(outDir) == TestPassed)
   }
 
   test("map:20:no-simplify") {
-    val inFile = BenchmarksDir / "map_20.txt"
-    val outDir = VhdlDir / "aetherling_map_20_test_no_simplify"
-    if (os.exists(outDir)) os.remove.all(outDir)
-    val args = Args(inFile = inFile, outDir = outDir, simplify = false)
-    val f = Compiler.compile(args)
-    val input = TestInput(
-      (0 until 10).flatMap(t =>
-        Seq(None, Some(VecLiteral((0 until 20).map(i => C(i + t)(U8)): _*)()))
-      )
+    runTest(
+      "map_20",
+      simplify = false,
+      input = StmLiteral(
+        (0 until 10).map(t =>
+          VecLiteral((0 until 20).map(i => C(i + t)(U8)): _*)()
+        ): _*
+      )(),
+      expectedOutput = StmLiteral(
+        (0 until 10).map(t =>
+          VecLiteral((0 until 20).map(i => C(i + t + 5)(U8)): _*)()
+        ): _*
+      )()
     )
-    val expectedOutput = StmLiteral(
-      (0 until 10).map(t =>
-        VecLiteral((0 until 20).map(i => C(i + t + 5)(U8)): _*)()
-      ): _*
-    )()
-    TestbenchGenerator.makeTestbench(
-      inputsByVar = Map(f.asInstanceOf[Function].param -> input),
-      out = expectedOutput,
-      dir = outDir
-    )
-    assert(TestRunner.testExistingProject(outDir) == TestPassed)
   }
 
   test("map:200") {
-    val inFile = BenchmarksDir / "map_200.txt"
-    val outDir = VhdlDir / "aetherling_map_200_test"
-    if (os.exists(outDir)) os.remove.all(outDir)
-    val args = Args(inFile = inFile, outDir = outDir)
-    val f = Compiler.compile(args)
-    val input =
-      TestInput(Seq(Some(VecLiteral((0 until 200).map(i => C(i)(U8)): _*)())))
-    val expectedOutput = StmLiteral(
-      VecLiteral((0 until 200).map(i => C(i + 5)(U8)): _*)()
-    )()
-    TestbenchGenerator.makeTestbench(
-      inputsByVar = Map(f.asInstanceOf[Function].param -> input),
-      out = expectedOutput,
-      dir = outDir
+    runTest(
+      "map_200",
+      simplify = true,
+      input = StmLiteral(VecLiteral((0 until 200).map(i => C(i)(U8)): _*)())(),
+      expectedOutput = StmLiteral(
+        VecLiteral((0 until 200).map(i => C(i + 5)(U8)): _*)()
+      )()
     )
-    assert(TestRunner.testExistingProject(outDir) == TestPassed)
   }
 
   test("map:200:no-simplify") {
-    val inFile = BenchmarksDir / "map_200.txt"
-    val outDir = VhdlDir / "aetherling_map_200_test_no_simplify"
-    if (os.exists(outDir)) os.remove.all(outDir)
-    val args = Args(inFile = inFile, outDir = outDir, simplify = false)
-    val f = Compiler.compile(args)
-    val input =
-      TestInput(Seq(Some(VecLiteral((0 until 200).map(i => C(i)(U8)): _*)())))
-    val expectedOutput = StmLiteral(
-      VecLiteral((0 until 200).map(i => C(i + 5)(U8)): _*)()
-    )()
-    TestbenchGenerator.makeTestbench(
-      inputsByVar = Map(f.asInstanceOf[Function].param -> input),
-      out = expectedOutput,
-      dir = outDir
+    runTest(
+      "map_200",
+      simplify = false,
+      input = StmLiteral(VecLiteral((0 until 200).map(i => C(i)(U8)): _*)())(),
+      expectedOutput = StmLiteral(
+        VecLiteral((0 until 200).map(i => C(i + 5)(U8)): _*)()
+      )()
     )
-    assert(TestRunner.testExistingProject(outDir) == TestPassed)
+  }
+
+  test("sum:1/840") {
+    runTest(
+      "sum_1_840",
+      simplify = true,
+      input = StmLiteral((0 until 840).map(t => C(t)(U32)): _*)(),
+      expectedOutput = StmLiteral(C((0 until 840).sum)(U32))()
+    )
+  }
+
+  test("sum:1/840:no-simplify") {
+    runTest(
+      "sum_1_840",
+      simplify = false,
+      input = StmLiteral((0 until 840).map(t => C(t)(U32)): _*)(),
+      expectedOutput = StmLiteral(C((0 until 840).sum)(U32))()
+    )
+  }
+
+  test("sum:1/210") {
+    runTest(
+      "sum_1_210",
+      simplify = true,
+      input = StmLiteral(
+        (0 until 210).map(i =>
+          VecLiteral((0 until 4).map(j => C(i + j)(U32)): _*)()
+        ): _*
+      )(),
+      expectedOutput = StmLiteral(
+        C((0 until 210).flatMap(i => (0 until 4).map(j => i + j)).sum)(U32)
+      )()
+    )
+  }
+
+  test("sum:1/210:no-simplify") {
+    runTest(
+      "sum_1_210",
+      simplify = false,
+      input = StmLiteral(
+        (0 until 210).map(i =>
+          VecLiteral((0 until 4).map(j => C(i + j)(U32)): _*)()
+        ): _*
+      )(),
+      expectedOutput = StmLiteral(
+        C((0 until 210).flatMap(i => (0 until 4).map(j => i + j)).sum)(U32)
+      )()
+    )
   }
 }
