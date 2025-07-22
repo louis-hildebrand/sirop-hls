ff665801d0671b31c4e0b287b9e028749695ab99
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-07-22 12:56:29 -0400

    Call BinOpTreeMaker in Optimizer

--- src/main/scala/mhir/optimize/Optimizer.scala
+++ src/main/scala/mhir/optimize/Optimizer.scala
@@ -1,12 +1,13 @@
 package mhir.optimize
 
 import mhir.ir._
+import mhir.optimize.{BinOpTreeMaker => BOTM}
 
 /** Top-level optimizer.
   */
 object Optimizer {
   def optimize(e: Expr): Expr = {
     val simplified = SafeSimplifier.simplify(e)
-    simplified
+    BOTM.makeBinOpTrees(simplified)
   }
 }
