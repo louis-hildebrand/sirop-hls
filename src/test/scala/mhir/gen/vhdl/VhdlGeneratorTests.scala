louis-hildebrand/sirop-hls

commit 1f61a59a99185d605d6ce574028e079e8af70029

Don't partially evaluate in VHDL generator
The VHDL generator previously partially evaluated inside VecBuild. The
problem is that this turns adder trees back into flat lists, which
renders the transformation introduced in the last few commits useless
for the Aetherling benchmarks.

src/main/scala/mhir/gen/vhdl/VhdlExprGenerator.scala
@@ -2,7 +2,6 @@ package mhir.gen.vhdl
 
 import mhir.ir._
 import mhir.ir.typecheck.TypeCheck
-import mhir.optimize.PartialEvalPass
 
 /** @param vhdl
   *   VHDL code for this expression
@@ -202,7 +201,7 @@ private object VhdlExprGenerator {
           val idxTyp = f.param.typ.asInstanceOf[TyAnyInt]
           val elems =
             (0 until n.toInt).map(i =>
-              PartialEvalPass.partialEval(FunCall(f, C(i)(idxTyp))())
+              f.body.subPreserveType(f.param -> C(i)(idxTyp))
             )
           exprToVhdl(VecLiteral(elems: _*)().tchk())
         } else {

commit 25605ce0c7086c083b6429117d4a21608d6b5adc

Fix bugs introduced by 1f61a59

src/main/scala/mhir/gen/vhdl/VhdlExprGenerator.scala
@@ -188,12 +188,30 @@ private object VhdlExprGenerator {
         val av = exprToVhdl(arg)
         VhdlExpr(s"${fv.vhdl}(${av.vhdl})", fv.decls ++ av.decls)
 
-      case VecLiteral(elems @ _*) =>
+      case v @ VecLiteral(elems @ _*) =>
         val vhdlElems = elems.map(exprToVhdl)
-        val assignments = vhdlElems.zipWithIndex
-          .map({ case (e, i) => s"$i => ${e.vhdl}" })
-          .mkString(", ")
-        VhdlExpr(s"($assignments)", vhdlElems.flatMap(e => e.decls))
+        val tempVar = {
+          val name = Param("vec")().name
+          val vec = vhdlElems.zipWithIndex
+            .map({ case (e, i) => s"$i => ${e.vhdl}" })
+            .mkString("(", ", ", ")")
+          mode match {
+            case NormalMode =>
+              Signal(
+                category = "Intermediate signals",
+                name = name,
+                typ = VhdlType(v.typ),
+                assignStmt = Some(s"$name <= $vec;")
+              )
+            case InFunctionMode =>
+              VhdlVariable(
+                name = name,
+                typ = VhdlType(v.typ),
+                assignStmt = s"$name := $vec;"
+              )
+          }
+        }
+        VhdlExpr(tempVar.name, tempVar +: vhdlElems.flatMap(e => e.decls))
       case VecBuild(len, f) =>
         if (len.freeVars().isEmpty) {
           // TODO: Use for-generate here instead?

src/main/scala/mhir/main/aetherling/Compiler.scala
@@ -3,6 +3,7 @@ package mhir.main.aetherling
 import mhir.gen.vhdl.VhdlGenerator
 import mhir.ir._
 import mhir.ir.Lowering.ExprLowering
+import mhir.ir.Uncurrier.Uncurry
 import mhir.ir.typecheck.TypeCheck
 import mhir.parse.AetherlingParser
 import mhir.optimize.{Optimizer => Opt}
@@ -84,7 +85,7 @@ object Compiler {
   }
 
   private def makeSynthesizable(e: Expr): Expr = {
-    inlineFunCalls(e).streamify()
+    uncurryBody(inlineFunCalls(e).streamify())
   }
 
   private def inlineFunCalls(e: Expr): Expr = {
@@ -99,6 +100,16 @@ object Compiler {
       case Function(x, body) =>
         Function(x, inlineFunCalls(body))()
       case e => e
+    }
+    result.tchk()
+  }
+
+  private def uncurryBody(e: Expr): Expr = {
+    val result = e match {
+      case Function(x, body) =>
+        Function(x, uncurryBody(body))()
+      case e =>
+        e.uncurry()
     }
     result.tchk()
   }

src/test/scala/mhir/gen/vhdl/VhdlGeneratorTests.scala
@@ -362,6 +362,31 @@ class VhdlGeneratorTests extends AnyFunSuite {
     assert(VhdlTestRunner.testExpr(s) == TestPassed)
   }
 
+  test("VecAccess(VecBuild(...), i)") {
+    val s =
+      StmBuild(2, VecAccess(VecBuild(3, U8 ::+ (i => 42 + i))(), 1)(), True)()
+        .tchk()
+        .lower()
+    assert(VhdlTestRunner.testExpr(s) == TestPassed)
+  }
+
+  test("f( (42, True, -1) )") {
+    val f = (U8, TyBool, I16) ::+ (x => x.__0)
+    val s =
+      StmBuild(2, f(Tuple(C(42)(U8), True, C(-1)(I16))()), True)()
+        .tchk()
+        .lower()
+    assert(VhdlTestRunner.testExpr(s) == TestPassed)
+  }
+
+  test("Tuple(10, 20, 30).__1") {
+    val s =
+      StmBuild(2, Tuple(C(10)(), C(20)(), C(30)())().__1, True)()
+        .tchk()
+        .lower()
+    assert(VhdlTestRunner.testExpr(s) == TestPassed)
+  }
+
   test("CurriedFunCall") {
     val n = 7
     val f = (U32, U32) ::+ (x => U32 ::+ (y => y + x.__0 * y + x.__1))