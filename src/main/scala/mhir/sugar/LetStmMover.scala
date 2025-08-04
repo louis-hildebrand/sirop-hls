cb5a29a4051ae0ed785c1880cf25ff2481d17e6e
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-08-04 18:16:57 -0400

    Implement transformation to move LetStm up in AST

---
+++ src/main/scala/mhir/sugar/LetStmMover.scala
@@ -0,0 +1,120 @@
+package mhir.sugar
+
+import mhir.ir._
+import mhir.ir.typecheck.TypeCheck
+
+/** Transformations for moving [[mhir.ir.LetStm]] around within an expression.
+  */
+object LetStmMover {
+
+  /** Move [[mhir.ir.LetStm]] up towards the root of the AST.
+    *
+    * @example
+    *   We want to turn an expression like this:
+    *   {{{
+    *       StmMap(
+    *           let s1 = (
+    *               let s0 = StmZip(input, StmCount(n)) in
+    *               StmMap(s0, (x, y) => x + y)
+    *           ) in
+    *           StmPrefix(s1, m),
+    *           x => x + 5
+    *       )
+    *   }}}
+    *   into something like
+    *   {{{
+    *       let s0 = StmZip(input, StmCount(n)) in
+    *       let s1 = StmMap(s0, (x, y) => x + y) in
+    *       StmMap(StmPrefix(s1, m), x => x + 5)
+    *   }}}
+    *   (However, this would all be done in terms of [[mhir.ir.StmBuild]], since
+    *   expressions must be lowered before this transformation can be applied.)
+    *
+    * @param e
+    *   the expression to transform.
+    */
+  def moveUp(e: Expr): Expr = {
+    require(
+      e.hasType,
+      "Expression must be type checked before moving LetStm up."
+        + s" (Found expression $e)"
+    )
+    require(
+      !e.contains(classOf[SyntaxSugar]),
+      "Syntax sugar must be removed before moving LetStm up."
+        + s" (Found expression $e)"
+      // ... otherwise, how would we know when it is legal to move the LetStm
+      // up? In some cases (e.g., StmMap, StmZip) we would want to pull LetStm
+      // out of an expression. But in other cases this would not be legal
+      // (e.g., inside a function, since the LetStm may depend on the function
+      // param).
+    )
+    val result = e match {
+      case s: StmBuild =>
+        def pullOutLet(s: StmBuild): Expr = {
+          val x = s.seedByVar
+            .find({
+              case (_, _: LetStm) => true
+              case _              => false
+            })
+            .map(_._1)
+          x match {
+            case Some(x) =>
+              val LetStm(y, in, out) = s.seedByVar(x).asInstanceOf[LetStm]
+              LetStm(
+                y,
+                in,
+                pullOutLet(
+                  StmBuild(
+                    s.n,
+                    s.data,
+                    s.valid,
+                    s.equations.map({ case (x2, (z, next)) =>
+                      if (x2 == x) {
+                        x2 -> (out, next)
+                      } else {
+                        x2 -> (z, next)
+                      }
+                    })
+                  )()
+                )
+              )()
+            case None =>
+              s
+          }
+        }
+        val withTransformedProducers =
+          StmBuild(
+            s.n,
+            s.data,
+            s.valid,
+            s.equations.map({ case (x, (z, next)) =>
+              x.typ match {
+                case TyData(_) => x -> (z, next)
+                case _: TyStm  => x -> (moveUp(z), next)
+                case _         => ???
+              }
+            })
+          )()
+        pullOutLet(withTransformedProducers)
+      case LetStm(x, in, out) =>
+        def pullOutLet(let: LetStm): Expr = {
+          let match {
+            case LetStm(x, LetStm(y, innerIn, innerOut), out) =>
+              val newY = y.freshCopy
+              val newInnerOut = innerOut.tchk().subPreserveType(y -> newY)
+              LetStm(newY, innerIn, pullOutLet(LetStm(x, newInnerOut, out)()))()
+            case let =>
+              let
+          }
+        }
+        val withTransformedChildren = LetStm(x, moveUp(in), moveUp(out))()
+        pullOutLet(withTransformedChildren)
+      case e =>
+        e.map(moveUp)
+    }
+    val typedResult = result.tchk()
+    assert(typedResult.typ == e.typ, "moving LetStm up should preserve type")
+    typedResult
+  }
+}
---
+++ src/test/scala/mhir/sugar/LetStmMoverTests.scala
@@ -0,0 +1,138 @@
+package mhir.sugar
+
+import mhir.ir.Lowering.ExprLowering
+import mhir.ir._
+import mhir.ir.typecheck.TypeCheck
+import org.scalatest.funsuite.AnyFunSuite
+
+class LetStmMoverTests extends AnyFunSuite {
+  test("InsideStmBuild") {
+    val n = 10
+    val input = Param("input")(TyStm(U8, n))
+    val s = Param("s")(TyStm(U8, n))
+    val lets = LetStm(s, input, LetStm(s, s, s)())()
+    val top = StmBuild(
+      n,
+      C(5)(U8) + StmData(s)(),
+      True,
+      Map[Param, (Expr, Expr)](
+        s -> (lets, True)
+      )
+    )().tchk().lower()
+    val actual = LetStmMover.moveUp(top)
+    val expected = LetStm(
+      s,
+      input,
+      LetStm(
+        s,
+        s,
+        StmBuild(
+          n,
+          Sum(C(5)(U8), StmData(s)())(),
+          True,
+          Map[Param, (Expr, Expr)](
+            s -> (s, True)
+          )
+        )()
+      )()
+    )().tchk()
+    assert(actual == expected)
+  }
+
+  test("let s = (let s = input in let s = s in StmZip(s, s)) in StmZip(s, s)") {
+    val n = 7
+    val s = Param("s")()
+    val input = Param("input")(TyStm(TyBool, n))
+    val zipped = (t: Type) => {
+      val s0 = Param("s0")(TyStm(t, n))
+      val s1 = Param("s1")(TyStm(t, n))
+      StmBuild(
+        n,
+        Tuple(StmData(s0)(), StmData(s1)())(),
+        True,
+        Map[Param, (Expr, Expr)](
+          s0 -> (s, True),
+          s1 -> (s, True)
+        )
+      )()
+    }
+    val e =
+      LetStm(
+        s,
+        LetStm(s, input, LetStm(s, s, zipped(TyBool))())(),
+        zipped((TyBool, TyBool))
+      )().tchk().lower()
+    val actual = LetStmMover.moveUp(e)
+    val expected =
+      LetStm(
+        s,
+        input,
+        LetStm(s, s, LetStm(s, zipped(TyBool), zipped((TyBool, TyBool)))())()
+      )().tchk()
+    assert(actual == expected)
+  }
+
+  test("VariableCapture") {
+    val n = 6
+    val a = Param("a")(TyStm(U8, n))
+    val b = Param("b")(TyStm(U8, n))
+    val i = Param("i")(U8)
+    val s0 = Param("s0")(TyStm(U8, n))
+    val s1 = Param("s1")(TyStm(U8, n))
+    // let a = (let b = ...) in StmZip(a, b) <-- b is free!
+    val original =
+      LetStm(
+        a,
+        LetStm(
+          b,
+          StmBuild(
+            n,
+            i,
+            True,
+            Map[Param, (Expr, Expr)](
+              i -> (C(42)(U8), C(1)(U8) + i)
+            )
+          )(),
+          b
+        )(),
+        StmBuild(
+          n,
+          Tuple(StmData(s0)(), StmData(s1)())(),
+          True,
+          Map[Param, (Expr, Expr)](
+            s0 -> (a, True),
+            s1 -> (b, True)
+          )
+        )()
+      )().tchk().lower()
+    val actual = LetStmMover.moveUp(original)
+    val expected = {
+      val b2 = Param("b")(TyStm(U8, n))
+      LetStm(
+        b2,
+        StmBuild(
+          n,
+          i,
+          True,
+          Map[Param, (Expr, Expr)](
+            i -> (C(42)(U8), Sum(C(1)(U8), i)())
+          )
+        )(),
+        LetStm(
+          a,
+          b2,
+          StmBuild(
+            n,
+            Tuple(StmData(s0)(), StmData(s1)())(),
+            True,
+            Map[Param, (Expr, Expr)](
+              s0 -> (a, True),
+              s1 -> (b, True)
+            )
+          )()
+        )()
+      )().tchk()
+    }
+    assert(actual == expected)
+  }
+}
