f30865564585626148719bff53f14c89c4c000c3
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-08-08 16:15:44 -0400

    Update debug package docs; mention it in README

--- src/main/scala/mhir/debug/package.scala
+++ src/main/scala/mhir/debug/package.scala
@@ -6,6 +6,9 @@ package mhir
   * It is sometimes useful to inspect the behaviour of a stream pipeline
   * step-by-step to understand an error. There are methods to do so in
   * [[Tracer]].
+  *
+  * Furthermore, it is nice to get traces in a graphical format. [[DotPrinter]]
+  * can convert a trace to an image using [[https://graphviz.org/ Graphviz]].
   */
 
 package object debug {
