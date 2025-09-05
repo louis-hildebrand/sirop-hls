76cd09ac954e06488dcadb72a13126d38b4543a2
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-09-05 16:49:26 -0400

    Always at least partially evaluate

--- src/main/scala/mhir/optimize/Optimizer.scala
+++ src/main/scala/mhir/optimize/Optimizer.scala
@@ -13,18 +13,28 @@ object Optimizer {
   def optimize(s: Expr, options: OptimizerOptions): Expr = {
     logger.trace(s"optimizing expression: $s")
 
-    val s0 = if (options.simplify) {
+    // No flag to skip this step because
+    //  (1) The partial evaluator is used everywhere (including by the type
+    //      checker, to check lengths!). Skipping this step would not completely
+    //      disable partial evaluation, and turning off partial evaluation at
+    //      the source (maybe with some kind of mutable kill switch) may break
+    //      things.
+    //  (2) If you don't at least partially evaluate, the code is so bad for
+    //      bigger examples (e.g., 1920x1080 conv2d) that simulation fails.
+    val s0 = PE.partialEval(s)
+
+    val s1 = if (options.simplify) {
       time("basic simplifications") {
-        SafeSimplifier.simplify(s)
+        SafeSimplifier.simplify(s0)
       }
     } else {
       logger.info(s"skipping basic simplification")
-      s
+      s0
     }
 
-    val s1 = if (options.fuse) {
+    val s2 = if (options.fuse) {
       time("greedy fusion") {
-        val fused = GreedyStmFuser.fuse(s0)
+        val fused = GreedyStmFuser.fuse(s1)
         // Partially evaluate in case there are some instances of LetStm which
         // now have at most one consumer
         val pe = PE.partialEval(fused)
@@ -32,34 +42,34 @@ object Optimizer {
       }
     } else {
       logger.info("skipping greedy fusion")
-      s0
+      s1
     }
 
-    val s2 = if (options.matchLatency) {
+    val s3 = if (options.matchLatency) {
       time("latency matching") {
-        StmLatencyMatcher.matchLatencies(s1)
+        StmLatencyMatcher.matchLatencies(s2)
       }
     } else {
       logger.info("skipping latency matching")
-      s1
+      s2
     }
 
     // I think the program is more readable like this.
     // I don't think a compiler flag is needed, since it shouldn't change the
     // generated hardware in any meaningful way.
-    val s3 = time("moving LetStm up") {
-      LetStmMover.moveUp(s2)
+    val s4 = time("moving LetStm up") {
+      LetStmMover.moveUp(s3)
     }
 
-    val s4 = if (options.balanceBinOpTrees) {
+    val s5 = if (options.balanceBinOpTrees) {
       time("balancing binop trees") {
-        BOTM.makeBinOpTrees(s3)
+        BOTM.makeBinOpTrees(s4)
       }
     } else {
       logger.info("skipping balancing binop trees")
-      s3
+      s4
     }
 
-    s4
+    s5
   }
 }
