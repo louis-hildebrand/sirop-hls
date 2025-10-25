556034ee4c5b73022534e39c8f7af24bde14fca7
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-10-25 17:53:05 -0400

    Add fmax info to ablation area plot

--- src/test/python/ablation_plot_resource_usage.py
+++ src/test/python/ablation_plot_resource_usage.py
@@ -5,7 +5,6 @@ This script plots the resource usages for the ablation study.
 """
 
 import math
-import sys
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tick
@@ -18,10 +17,15 @@ import lib.plt_utils as pu
 from lib.program_variant import ProgramVariant
 from lib.resource_usage import ResourceUsage
 
-BRAM_YMAX = 20
+BRAM_YMAX = 23
+# pylint: disable-next=line-too-long
+WARNING = r"\textbf{{\Large $\triangle$}\hspace{-0.785em}\raisebox{0.2em}{\scriptsize!}}\hspace{0.5em}"
 
 
-def plot_resource_usages(results: dict[ProgramVariant, ResourceUsage]) -> None:
+def plot_resource_usages(
+    results: dict[ProgramVariant, ResourceUsage],
+    fmax_results: dict[ProgramVariant, float],
+) -> None:
     """
     Plot resource usage for each program.
     """
@@ -35,6 +39,7 @@ def plot_resource_usages(results: dict[ProgramVariant, ResourceUsage]) -> None:
         raise ValueError("Nothing to plot.")
     plt.rcParams.update({
         "text.usetex": True,
+        "text.latex.preamble": r"\usepackage{color}",
         "font.family": "Times New Roman",
         "font.size": 8,
     })
@@ -84,24 +89,6 @@ def plot_resource_usages(results: dict[ProgramVariant, ResourceUsage]) -> None:
             hatch_linewidth=aps.HATCH_WIDTH,
         )
         artists.append(artist)
-        # labels = []
-        # for p in program_names:
-        #     alm = results[ProgramVariant(p, lvl)].alm
-        #     baseline = results[ProgramVariant(p, BASELINE_LVL)].alm
-        #     percent_change = (alm - baseline) / baseline
-        #     if percent_change >= 0:
-        #         label = f"+{percent_change:.0%}"
-        #     else:
-        #         label = f"{percent_change:.0%}"
-        #     label = label.replace("%", r"\%")
-        #     if label == r"+0\%":
-        #         label = ""
-        #     labels.append(label)
-        # alm_ax.bar_label(
-        #     artist,
-        #     labels=labels,
-        #     padding=3,
-        # )
         # BRAM usage
         ys = []
         for p in program_names:
@@ -142,6 +129,12 @@ def plot_resource_usages(results: dict[ProgramVariant, ResourceUsage]) -> None:
             label_type="center",
             color="white",
         )
+        # Fmax warning labels
+        for i, p in enumerate(program_names):
+            pv = ProgramVariant(p, lvl)
+            if pv not in fmax_results or fmax_results[pv] < c.TARGET_FREQ:
+                alm_ax.annotate(WARNING, (xs[i], 0.25), ha="center", color="red")
+                bram_ax.annotate(WARNING, (xs[i], 0.25), ha="center", color="red")
     # Display settings
     alm_ax.set_xlim(xlim)
     alm_ax.set_xticks(
@@ -155,7 +148,8 @@ def plot_resource_usages(results: dict[ProgramVariant, ResourceUsage]) -> None:
     alm_ax.set_ylabel("\\% change\nALM usage")
     alm_ax.set_yscale("symlog")
     alm_ax.yaxis.set_major_formatter(tick.PercentFormatter(1))
-    alm_ax.set_ylim(-0.5, 40)
+    alm_ax.set_yticks([-1, 0, 1, 10])
+    alm_ax.set_ylim(-1, 40)
     bram_ax.tick_params(axis="x", which="both", length=0)
     bram_ax.set_ylabel("\\% change\nBRAM usage")
     bram_ax.set_yscale("symlog")
@@ -169,16 +163,22 @@ def plot_resource_usages(results: dict[ProgramVariant, ResourceUsage]) -> None:
         transform=fig.transFigure, figure=fig
     )
     fig.patches.extend([down_arrow])
-    legend_cols = math.ceil( (len(aps.LEVELS_TO_PLOT) + 1) / aps.LEGEND_ROWS )
-    legend_labels=[lvl.explanation for lvl in [aps.BASELINE_LVL] + aps.LEVELS_TO_PLOT]
-    legend_handles=[baseline_artist] + artists
+    legend_cols = 1 + math.ceil( (len(aps.LEVELS_TO_PLOT) + 1) / aps.LEGEND_ROWS )
+    legend_labels=(
+        [lvl.explanation for lvl in [aps.BASELINE_LVL] + aps.LEVELS_TO_PLOT]
+            + ["timing requirements not met"]
+    )
+    empty_handle, *_ = alm_ax.plot([0], [0], linestyle="")
+    legend_handles=[baseline_artist] + artists + [empty_handle]
     fig.legend(
         labels=pu.flip(legend_labels, legend_cols),
         handles=pu.flip(legend_handles, legend_cols),
         loc="upper center",
-        bbox_to_anchor=(0.5, 0),
+        bbox_to_anchor=(0.5, -0.05),
         ncols=legend_cols,
+        handlelength=1.5,
     )
+    fig.text(0.71, -0.1375, WARNING, color="red", zorder=1000)
     fig.savefig(c.ABLATION_RESOURCE_USAGE_PDF, bbox_inches="tight")
 
 
@@ -186,10 +186,9 @@ def main() -> None:
     """
     The program entry point.
     """
-    results = crud.read_valid_resource_usage_results(c.ABLATION_RESOURCE_USAGE_CSV)
-    if not results:
-        sys.exit("No results to plot.")
-    plot_resource_usages(results)
+    area_results = crud.read_valid_resource_usage_results(c.ABLATION_RESOURCE_USAGE_CSV)
+    fmax_results = crud.read_valid_fmax(c.ABLATION_FMAX_CSV)
+    plot_resource_usages(area_results, fmax_results)
 
 
 if __name__ == "__main__":
