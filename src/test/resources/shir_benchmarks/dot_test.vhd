ca0377179a5634d5bd73b62530a6212b358b1c06
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-11-05 14:12:07 -0500

    Measure latency for SHIR dot benchmark

--- src/test/resources/shir_benchmarks/dot_test.vhd
+++ src/test/resources/shir_benchmarks/dot_test.vhd
@@ -8,25 +8,32 @@ end testbench;
 
 architecture tb of testbench is
 
-    constant CLK_CYCLE   : time := 20 ns;
-    signal   test_done   : boolean := false;
-    signal   clk         : std_logic := '1';
-    signal   reset       : std_logic := '0';
-    signal   t           : integer := -1;
+    constant CLK_CYCLE : time := 20 ns;
+    signal   test_done : boolean := false;
+    signal   clk       : std_logic := '1';
+    signal   reset     : std_logic := '0';
+    signal   t         : integer := -1;
 
     -- Easier debugging
-    signal   data_t     : unsigned(7 downto 0);
+    signal   data_t : unsigned(25 downto 0);
 
     -- Input stream: I0
-    signal I0_data  : std_logic_vector(7 downto 0);
+    signal I0_data  : std_logic_vector(15 downto 0);
     signal I0_last  : std_logic_vector(0 downto 0);
     signal I0_valid : std_logic;
     signal I0_ready : std_logic_vector(1 downto 0);
+
+    -- Input stream: I1
+    signal I1_data  : std_logic_vector(15 downto 0);
+    signal I1_last  : std_logic_vector(0 downto 0);
+    signal I1_valid : std_logic;
+    signal I1_ready : std_logic_vector(1 downto 0);
+
     -- Expected outputs
-    signal data     : std_logic_vector(7 downto 0);
-    signal last     : std_logic_vector(0 downto 0);
-    signal valid    : std_logic;
-    signal ready    : std_logic_vector(1 downto 0);
+    signal data  : std_logic_vector(25 downto 0);
+    signal last  : std_logic_vector(-1 downto 0);
+    signal valid : std_logic;
+    signal ready : std_logic_vector(0 downto 0);
 
     ----------------------------------------------------------------------------
     -- Test case 0
@@ -34,14 +41,16 @@ architecture tb of testbench is
 
     signal test_0_start : boolean := false;
     signal test_0_I0_inputs_done : boolean := false;
+    signal test_0_I1_inputs_done : boolean := false;
     signal test_0_outputs_done : boolean := false;
 
 begin
 
     DUT : entity work.top port map(
         clk => clk, reset => reset,
-        p0_in_data => I0_data, p0_in_valid => I0_valid, p0_in_last => I0_last, p0_out_ready => I0_ready,
-        p1_out_data => data, p1_out_valid => valid, p1_out_last => last, p1_in_ready => ready
+        p0_in_data => I0_data, p0_in_last => I0_last, p0_in_valid => I0_valid, p0_out_ready => I0_ready,
+        p1_in_data => I1_data, p1_in_last => I1_last, p1_in_valid => I1_valid, p1_out_ready => I1_ready,
+        p2_out_data => data, p2_out_last => last, p2_out_valid => valid, p2_in_ready => ready
     );
 
     data_t <= unsigned(data);
@@ -73,19 +82,13 @@ begin
         variable i : integer;
     begin
         I0_valid <= 'Z';
-        I0_last <= "Z";
         I0_data <= (others => 'Z');
         wait until test_0_start;
 
-        for i in 0 to 199 loop
+        for i in 0 to 839 loop
             wait until falling_edge(clk); -- prepare input well before the next rising edge
             I0_valid <= '1';
-            if i = 199 then
-                I0_last <= "1";
-            else
-                I0_last <= "0";
-            end if;
-            I0_data <= std_logic_vector(to_unsigned(i, 8));
+            I0_data <= std_logic_vector(to_unsigned(i mod 16, 16));
             wait until rising_edge(clk) and (test_0_outputs_done or I0_ready /= "00"); -- must wait for the design to accept the input
         end loop;
 
@@ -94,21 +97,38 @@ begin
         wait;
     end process;
 
-    -- Check outputs
-    out_check_0 : process
-        variable expected : std_logic_vector(7 downto 0);
+    -- Generate inputs (I1)
+    process
         variable i : integer;
     begin
-        ready <= "ZZ";
+        I1_valid <= 'Z';
+        I1_data <= (others => 'Z');
         wait until test_0_start;
 
-        for i in 0 to 199 loop
-            -- Wait until falling edge to give output time to settle down (probably not necessary, but just in case)
-            wait until falling_edge(clk) and valid = '1';
-            ready <= "11";
-            expected        := std_logic_vector(to_unsigned(i + 5, 8));
-            assert(data = expected) report "Wrong data at t = " & integer'image(t) & ".";
+        for i in 0 to 840 loop
+            wait until falling_edge(clk); -- prepare input well before the next rising edge
+            I1_valid <= '1';
+            I1_data <= std_logic_vector(to_unsigned((15 - i - 8) mod 16, 16));
+            wait until rising_edge(clk) and (test_0_outputs_done or I1_ready /= "00"); -- must wait for the design to accept the input
         end loop;
+
+        test_0_I1_inputs_done <= true;
+        report "Finished giving inputs for test case 0, parameter I1." severity note;
+        wait;
+    end process;
+
+    -- Check outputs
+    out_check_0 : process
+        variable expected : std_logic_vector(25 downto 0);
+    begin
+        ready <= "Z";
+        wait until test_0_start;
+
+        -- Wait until falling edge to give output time to settle down (probably not necessary, but just in case)
+        wait until falling_edge(clk) and valid = '1';
+        ready <= "1";
+        expected := std_logic_vector(to_unsigned(55800, 26));
+        assert(data = expected) report "Wrong data at t = " & integer'image(t) & ".";
 
         report "LATENCY: " & integer'image(t) & " cycles" severity note;
 
@@ -123,7 +143,7 @@ begin
             procedure run_test_0 is
             begin
                 test_0_start <= true;
-                wait until test_0_I0_inputs_done and test_0_outputs_done;
+                wait until test_0_I0_inputs_done and test_0_I1_inputs_done and test_0_outputs_done;
                 report "Test 0 done." severity note;
             end procedure run_test_0;
     begin
