564b1b2133459b8d8bc2e6b8855bb2bf448ed81c
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2026-01-20 20:19:53 -0500

    Synthesize Aetherling matrix-matrix multiplication benchmarks

--- src/test/sh/extract_dsp_count.sh
+++ src/test/sh/extract_dsp_count.sh
@@ -9,7 +9,7 @@ if [[ "$#" -ge 1 ]]; then
     f="output_files_$1/${1}.fit.summary"
 elif [[ -f 'output_files/top.fit.summary' ]]; then
     f='output_files/top.fit.summary'
-elif [[ -f 'Top.fit.summary' ]]; then
+elif [[ -f 'output_files/Top.fit.summary' ]]; then
     f='output_files/Top.fit.summary'
 else
     >&2 echo "Could not find resource usage report."
