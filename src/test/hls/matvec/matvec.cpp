14585985bfac95efb80b622fd3059f21aab21239
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2026-02-27 17:26:25 -0500

    Improve C++ implementation for matvec

--- src/test/hls/matvec/matvec.cpp
+++ src/test/hls/matvec/matvec.cpp
@@ -1,30 +1,28 @@
 #include "HLS/hls.h"
-#include <stdio.h>
 #include <stdint.h>
-
-using namespace ihc;
+#include <stdio.h>
 
 constexpr int N = 256;
 
-template<unsigned SystemID> class InputPipeID {};
-template<unsigned SystemID> class OutputPipeID {};
+template<unsigned SystemID> class PipeID {};
 
-constexpr int A = 0;
-constexpr int B = 1;
-constexpr int C = 2;
-component void matvec(
-    ihc::pipe<class InputPipeID<A>, uint16_t> &mat,
-    ihc::pipe<class InputPipeID<B>, uint16_t> &vec,
-    ihc::pipe<class OutputPipeID<C>, uint16_t> &out
-) {
+ihc::pipe<class PipeID<0>, uint16_t> mat;
+ihc::pipe<class PipeID<1>, uint16_t> vec;
+ihc::pipe<class PipeID<2>, uint16_t> out;
+
+component void matvec() {
     uint16_t vec_arr[N];
-    for (int i = 0; i < N; i++) {
-        vec_arr[i] = vec.read();
-    }
     for (int i = 0; i < N; i++) {
         uint16_t sum = 0;
         for (int j = 0; j < N; j++) {
-            sum += mat.read() * vec_arr[j];
+            uint16_t v;
+            if (i == 0) {
+                v = vec.read();
+                vec_arr[j] = v;
+            } else {
+                v = vec_arr[j];
+            }
+            sum += mat.read() * v;
         }
         out.write(sum);
     }
@@ -42,20 +40,16 @@ int main() {
         vec_arr[i] = i % 16;
     }
 
-    ihc::pipe<class InputPipeID<A>, uint16_t> mat_stm;
-    ihc::pipe<class InputPipeID<B>, uint16_t> vec_stm;
-    ihc::pipe<class OutputPipeID<C>, uint16_t> out_stm;
-
     for (int i = 0; i < N; i++) {
         for (unsigned int j = 0; j < N; j++) {
-            mat_stm.write(mat_arr[i][j]);
+            mat.write(mat_arr[i][j]);
         }
     }
     for (int i = 0; i < N; i++) {
-        vec_stm.write(vec_arr[i]);
+        vec.write(vec_arr[i]);
     }
 
-    matvec(mat_stm, vec_stm, out_stm);
+    matvec();
 
     uint16_t expected[N];
     for (int i = 0; i < N; i++) {
@@ -67,7 +61,7 @@ int main() {
 
     bool pass = true;
     for (int i = 0; i < N; i++) {
-        uint16_t result = out_stm.read();
+        uint16_t result = out.read();
         if (result != expected[i]) {
             printf("ERROR: Expected %u, found %u\n", expected[i], result);
             pass = false;
