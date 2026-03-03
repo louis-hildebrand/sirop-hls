d960137b750da527a21eba3c740d2a63addf9f49
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2026-03-03 15:13:35 -0500

    Avoid fork-join in Intel HLS benchmarks

--- src/test/hls/sobel/sobel.cpp
+++ src/test/hls/sobel/sobel.cpp
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-using namespace ihc;
-
 constexpr int WIDTH = 1920;
 constexpr int HEIGHT = 1080;
 constexpr int32_t KERNEL_X[3][3] = {
@@ -19,16 +17,8 @@ constexpr int32_t KERNEL_Y[3][3] = {
 };
 
 template<unsigned SystemID> class PipeID {};
-ihc::pipe<PipeID<0>, int32_t> pipe_a;
-/* Some buffering is needed to get the throughput where we want it.
- * It's not clear why this is; the two branches look to me like they should have
- * identical latency.
- */
-ihc::stream<int32_t, ihc::buffer<4>> pipe_b;
-ihc::stream<int32_t, ihc::buffer<4>> pipe_c;
-ihc::stream<int32_t> pipe_d;
-ihc::stream<int32_t> pipe_e;
-ihc::pipe<PipeID<6>, int32_t> pipe_f;
+ihc::pipe<PipeID<0>, int32_t> pipe_in;
+ihc::pipe<PipeID<1>, int32_t> pipe_out;
 
 template<unsigned int img_width, unsigned int win_width, unsigned int win_height>
 class LineBuffer2D {
@@ -57,97 +47,49 @@ public:
     }
 };
 
-void fork_img() {
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            int32_t x = pipe_a.read();
-            pipe_b.write(x);
-            pipe_c.write(x);
-        }
-    }
-}
-
-void compute_gx() {
-    LineBuffer2D<WIDTH, 3, 3> buffer;
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            buffer.shift(pipe_b.read());
-            if (i >= 2 && j >= 2) {
-                int32_t window[3][3] = {
-                    buffer.big_buffer[0][0], buffer.big_buffer[0][1], buffer.big_buffer[0][2],
-                    buffer.big_buffer[1][0], buffer.big_buffer[1][1], buffer.big_buffer[1][2],
-                    buffer.small_buffer[0], buffer.small_buffer[1], buffer.small_buffer[2]
-                };
-                int32_t sum = 0;
-                #pragma unroll
-                for (int i = 0; i < 3; i++) {
-                    #pragma unroll
-                    for (int j = 0; j < 3; j++) {
-                        sum += KERNEL_X[i][j] * window[i][j];
-                    }
-                }
-                pipe_d.write(sum);
-            }
-        }
-    }
-}
-
-void compute_gy() {
-    LineBuffer2D<WIDTH, 3, 3> buffer;
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            buffer.shift(pipe_c.read());
-            if (i >= 2 && j >= 2) {
-                int32_t window[3][3] = {
-                    buffer.big_buffer[0][0], buffer.big_buffer[0][1], buffer.big_buffer[0][2],
-                    buffer.big_buffer[1][0], buffer.big_buffer[1][1], buffer.big_buffer[1][2],
-                    buffer.small_buffer[0], buffer.small_buffer[1], buffer.small_buffer[2]
-                };
-                int32_t sum = 0;
-                #pragma unroll
-                for (int i = 0; i < 3; i++) {
-                    #pragma unroll
-                    for (int j = 0; j < 3; j++) {
-                        sum += KERNEL_Y[i][j] * window[i][j];
-                    }
-                }
-                pipe_e.write(sum);
-            }
-        }
-    }
-}
-
-void compute_norm() {
-    for (int i = 0; i < HEIGHT-2; i++) {
-        for (int j = 0; j < WIDTH-2; j++) {
-            int32_t x = pipe_d.read();
-            int32_t y = pipe_e.read();
-            int32_t norm_squared = x*x + y*y;
-            int32_t lo = 0;
-            int32_t hi = 65535;
-            #pragma unroll
-            for (int _ = 0; _ < 16; _++) {
-                int32_t mid = (lo + hi + 1) >> 1;
-                if (mid*mid <= norm_squared) {
-                    lo = mid;
-                } else /* mid*mid > norm_squared */ {
-                    hi = mid - 1;
-                }
-            }
-            pipe_f.write(lo);
-        }
-    }
-}
-
 component void sobel() {
-    ihc::launch<fork_img>();
-    ihc::launch<compute_gx>();
-    ihc::launch<compute_gy>();
-    ihc::launch<compute_norm>();
-    ihc::collect<fork_img>();
-    ihc::collect<compute_gx>();
-    ihc::collect<compute_gy>();
-    ihc::collect<compute_norm>();
+    LineBuffer2D<WIDTH, 3, 3> buffer;
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            buffer.shift(pipe_in.read());
+            if (i >= 2 && j >= 2) {
+                int32_t window[3][3] = {
+                    buffer.big_buffer[0][0], buffer.big_buffer[0][1], buffer.big_buffer[0][2],
+                    buffer.big_buffer[1][0], buffer.big_buffer[1][1], buffer.big_buffer[1][2],
+                    buffer.small_buffer[0], buffer.small_buffer[1], buffer.small_buffer[2]
+                };
+                int32_t gx = 0;
+                #pragma unroll
+                for (int i = 0; i < 3; i++) {
+                    #pragma unroll
+                    for (int j = 0; j < 3; j++) {
+                        gx += KERNEL_X[i][j] * window[i][j];
+                    }
+                }
+                int32_t gy = 0;
+                #pragma unroll
+                for (int i = 0; i < 3; i++) {
+                    #pragma unroll
+                    for (int j = 0; j < 3; j++) {
+                        gy += KERNEL_Y[i][j] * window[i][j];
+                    }
+                }
+                int32_t norm_squared = gx*gx + gy*gy;
+                int32_t lo = 0;
+                int32_t hi = 65535;
+                #pragma unroll
+                for (int _ = 0; _ < 16; _++) {
+                    int32_t mid = (lo + hi + 1) >> 1;
+                    if (mid*mid <= norm_squared) {
+                        lo = mid;
+                    } else /* mid*mid > norm_squared */ {
+                        hi = mid - 1;
+                    }
+                }
+                pipe_out.write(lo);
+            }
+        }
+    }
 }
 
 int main() {
@@ -166,7 +108,7 @@ int main() {
     printf("Sending data to input stream...\n");
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            pipe_a.write(in_arr[i][j]);
+            pipe_in.write(in_arr[i][j]);
         }
     }
 
@@ -206,7 +148,7 @@ int main() {
     bool pass = true;
     for (int i = 0; i < HEIGHT-2; i++) {
         for (int j = 0; j < WIDTH-2; j++) {
-            int32_t result = pipe_f.read();
+            int32_t result = pipe_out.read();
             if (result != expected[i][j]) {
                 printf("ERROR(%u,%u): Expected %u, found %u\n", i, j, expected[i][j], result);
                 pass = false;
@@ -222,5 +164,4 @@ int main() {
     }
 
     return 0;
-
 }
