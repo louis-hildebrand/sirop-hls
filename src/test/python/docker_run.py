c3d91d09d65d5224c389cc970461801fcd1eb45a
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2026-05-07 10:46:26 -0400

    Update docker_run.py
    The argument parser was getting confused about which flags are for the
    Python script and which are for inside the docker container. I guess
    it's better to just pass the whole command as a single string rather
    than accepting multiple separate args and concatenating them before
    handing them off to docker.

--- src/test/python/docker_run.py
+++ src/test/python/docker_run.py
@@ -14,18 +14,19 @@ import lib.constants as c
 DEFAULT_IMAGE = "louishildebrand/sirop-lctes26-artifact"
 
 
-def main(args: list[str], image: str, skip_chmod: bool) -> None:
+def main(command: str, image: str, skip_chmod: bool) -> None:
     """
     Script entry point.
     """
-    container_command = " ".join([f"'{a}'" for a in args])
-    if not skip_chmod:
+    if skip_chmod:
+        container_command = command
+    else:
         container_command = (
-            f"{{ {container_command}; }}"
+            f"{{ {command}; }}"
             " && chmod -R 777 /sirop/src/test/vhdl"
             " && chmod -R 777 /sirop/src/test/verilog"
         )
-    command = [
+    docker_command = [
         "sudo", "docker", "run",
         "--rm",
         "--mount", f"type=bind,src={c.VHDL_DIR.as_posix()},dst=/sirop/src/test/vhdl",
@@ -33,7 +34,7 @@ def main(args: list[str], image: str, skip_chmod: bool) -> None:
         image,
         "/bin/bash", "-c", container_command,
     ]
-    subprocess.run(command, check=True)
+    subprocess.run(docker_command, check=True)
 
 
 def parse_args() -> Namespace:
@@ -44,9 +45,8 @@ def parse_args() -> Namespace:
         description="runs the given command(s) in a docker container"
     )
     parser.add_argument(
-        "args",
-        nargs="+",
-        help="the command(s) to run",
+        "command",
+        help="the command to run",
     )
     parser.add_argument(
         "--image",
@@ -68,7 +68,7 @@ def parse_args() -> Namespace:
 if __name__ == "__main__":
     _args = parse_args()
     main(
-        args=_args.args,
+        command=_args.command,
         image=_args.image,
         skip_chmod=_args.skip_chmod,
     )
