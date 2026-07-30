f528efe03186206996a5fdac3fae4a683b1626a3
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2026-07-30 12:47:37 -0400

    Simplify dsp_systolic_{un,}signed.vhd
    It turns out the "formal part" (left-hand side) in a port map
    association can be a function call or type cast, as in
    
    ```
    entity work.foo
    port map(
        signed(an_out_port) => an_out_port_signed
    )
    ```
    
    which converts `an_out_port` to signed before assigning to
    `an_out_port_signed`. This makes my VHDL code a bit more concise.

--- src/main/resources/mhir/gen/vhdl/agilex7/dsp_systolic_signed.vhd
+++ src/main/resources/mhir/gen/vhdl/agilex7/dsp_systolic_signed.vhd
@@ -26,10 +26,6 @@ entity dsp_systolic_signed is
 end entity;
 
 architecture structural of dsp_systolic_signed is
-
-    signal chainout_slv : std_logic_vector(43 downto 0);
-    signal result_slv   : std_logic_vector(RESULT_WIDTH-1 downto 0);
-
 begin
 
     DSP : entity work.dsp_systolic
@@ -44,18 +40,15 @@ begin
             ENABLE_CHAININ  => ENABLE_CHAININ
         )
         port map(
-            clk      => clk,
-            ena      => ena,
-            ax       => std_logic_vector(ax),
-            ay       => std_logic_vector(ay),
-            bx       => std_logic_vector(bx),
-            by       => std_logic_vector(by),
-            chainin  => std_logic_vector(chainin),
-            chainout => chainout_slv,
-            result   => result_slv
+            clk              => clk,
+            ena              => ena,
+            ax               => std_logic_vector(ax),
+            ay               => std_logic_vector(ay),
+            bx               => std_logic_vector(bx),
+            by               => std_logic_vector(by),
+            chainin          => std_logic_vector(chainin),
+            signed(chainout) => chainout,
+            signed(result)   => result
         );
-
-    chainout <= signed(chainout_slv);
-    result <= signed(result_slv);
 
 end architecture;
